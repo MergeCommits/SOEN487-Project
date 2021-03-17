9f4613c9e9c196a53e10dacf4ad5d2752fc70c2c
Mark <markjarjour@gmail.com>
2021-03-17 15:30:37 -0400

    Added ability to delete artwork.

--- client/edit.php
+++ client/edit.php
@@ -29,31 +29,49 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $response = curl_exec($curl);
 
     curl_close($curl);
-//    echo $response;
 
     // ---
+
     $file = $_FILES['file'];
-    $curlFile = new CURLFILE($file['tmp_name']);
-    $curlFile->setMimeType($file['type']);
+    if ($file['tmp_name'] !== '') {
+        $curlFile = new CURLFILE($file['tmp_name']);
+        $curlFile->setMimeType($file['type']);
 
-    $curl = curl_init();
+        $curl = curl_init();
 
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => 'http://localhost:8080/myapp/album/cover/update/' . $isrc,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS => array('file'=> $curlFile),
-    ));
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'http://localhost:8080/myapp/album/cover/update/' . $isrc,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array('file' => $curlFile),
+        ));
 
-    $response = curl_exec($curl);
+        $response = curl_exec($curl);
 
-    curl_close($curl);
-//    echo $response;
+        curl_close($curl);
+    } else if (isset($_POST['del'])) {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'http://localhost:8080/myapp/album/cover/delete/' . $isrc,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'DELETE'
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+    }
 
     header("Location: http://localhost/client/view.php?isrc=$isrc");
     die();
